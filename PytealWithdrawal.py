3b9e3098653ad7f53ce4d25bf212ada3ba432bf3
zeredogo <zeredogo@gmail.com>
2022-10-06 00:52:16 +0100

    updating commit

--- PytealWithdrawal.py
+++ PytealWithdrawal.py
@@ -6,4 +6,5 @@ def Bank_Account(receiver):
     is_payment = Txn.type_enum() ==TxnType.payment
     is_singletransaction = Global.group_size() == int(1)
     is_receiver = Txn.receiver() == Addr(receiver)
-    no_closeout_address =
+    no_closeout_address = Txn.close_remainder_to() == Global.zero_address()
+    no_rekeying = Txn.close_remainder_to() == Global.zero_address()
